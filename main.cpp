5fba53b7159ac6f94aec0eb14666c9e10ad2e2da
Kuruyia <alexandresollier@gmail.com>
2020-05-30 22:20:29 +0200

    Added memory printer to main window

--- main.cpp
+++ main.cpp
@@ -21,12 +21,43 @@ void printRegisters(sf::RenderTarget &renderTarget, const gbtest::LR35902 &cpu, 
 
     std::stringstream sstr;
     sstr << std::hex
-            << "A: " << std::setw(2) << std::setfill('0') << (int)cpu.getRegisters().a << "h" << std::endl
+            << "A:  " << std::setw(2) << std::setfill('0') << (int)cpu.getRegisters().a << "h" << std::endl
             << "BC: " << std::setw(4) << std::setfill('0') << cpu.getRegisters().bc << "h" << std::endl
             << "DE: " << std::setw(4) << std::setfill('0') << cpu.getRegisters().de << "h" << std::endl
             << "HL: " << std::setw(4) << std::setfill('0') << cpu.getRegisters().hl << "h" << std::endl
             << "SP: " << std::setw(4) << std::setfill('0') << cpu.getRegisters().sp << "h" << std::endl
             << "PC: " << std::setw(4) << std::setfill('0') << cpu.getRegisters().pc << "h" << std::endl;
+
+    text.setString(sstr.str());
+    renderTarget.draw(text);
+}
+
+void printMemory(sf::RenderTarget &renderTarget, const gbtest::Bus &bus, const uint16_t &startAddr, const uint16_t &nbLines,
+        const sf::Vector2f &pos)
+{
+    sf::Font font;
+    font.loadFromFile("UbuntuMono-Regular.ttf");
+
+    sf::Text text;
+    text.setFont(font);
+    text.setCharacterSize(16);
+    text.setPosition(pos);
+
+    std::stringstream sstr;
+    sstr << std::hex << "Addr ";
+    for (unsigned i = 0; i <= 0xF; ++i)
+        sstr << std::setw(2) << i << ' ';
+
+    sstr << std::endl;
+    for (unsigned i = 0; i <= nbLines; ++i)
+    {
+        const uint16_t currBaseAddr = startAddr + (i * 0x10);
+        sstr << std::setfill('0') << std::setw(4) << currBaseAddr << ' ';
+        for (unsigned j = 0; j <= 0xF; ++j)
+            sstr << std::setw(2) << (int)bus.read(currBaseAddr + j) << ' ';
+
+        sstr << std::endl;
+    }
 
     text.setString(sstr.str());
     renderTarget.draw(text);
@@ -51,8 +82,11 @@ int main()
         window.clear();
 
         printRegisters(window, cpu, {0, 0});
+        printMemory(window, bus, 0, 0xF, {0, 150});
 
         window.display();
+
+        sf::sleep(sf::microseconds(13));
     }
 
     return 0;
