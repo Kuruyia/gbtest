3d83a443f91fc3e0900c060e3d381cff6f28d12f
Kuruyia <8174691+kuruyia@users.noreply.github.com>
2021-09-10 00:04:48 +0200

    OAMDMA: Implement transfer logic

--- src/ppu/oam/OAMDMA.cpp
+++ src/ppu/oam/OAMDMA.cpp
@@ -1,15 +1,62 @@
 #include "OAMDMA.h"
 
+gbtest::OAMDMA::OAMDMA(gbtest::Bus& bus)
+        : m_transferring(false)
+        , m_currentAddressLow(0x00)
+        , m_sourceAddressHigh(0x00)
+        , m_bus(bus)
+{
+
+}
+
+void gbtest::OAMDMA::startTransfer(uint8_t startAddressHigh)
+{
+    // We can't start another transfer if one is already in progress
+    if (m_transferring) { return; }
+
+    // Only allow start source address MSB from 00h to DFh
+    if (startAddressHigh > 0xDF) {
+        m_sourceAddressHigh = startAddressHigh;
+        return;
+    }
+
+    // Start the transfer
+    m_transferring = true;
+    m_currentAddressLow = 0x00;
+    m_sourceAddressHigh = startAddressHigh;
+}
+
+bool gbtest::OAMDMA::isTransferring() const
+{
+    return m_transferring;
+}
+
 void gbtest::OAMDMA::tick()
 {
-    // TODO: Handle the transfer one is in progress
+    // Only proceed if a transfer is ongoing
+    if (!m_transferring) { return; }
+
+    // Transfer a byte at each tick
+    const uint16_t sourceAddress = (m_sourceAddressHigh << 8) | m_currentAddressLow;
+    const uint16_t destinationAddress = 0xFE00 | m_currentAddressLow;
+
+    m_bus.write(destinationAddress, m_bus.read(sourceAddress, BusRequestSource::OAMDMA), BusRequestSource::OAMDMA);
+
+    // Increment the current address low byte, check if the transfer has finished
+    ++m_currentAddressLow;
+
+    if (m_currentAddressLow > 0x9F) {
+        m_transferring = false;
+    }
 }
 
 bool gbtest::OAMDMA::read(uint16_t addr, uint8_t& val, gbtest::BusRequestSource requestSource) const
 {
     // OAM DMA only uses address FF46h
     if (addr == 0xFF46) {
-        // TODO: Compute the register value, return it
+        // Return the current source address MSB
+        val = m_sourceAddressHigh;
+
         return true;
     }
 
@@ -20,7 +67,9 @@ bool gbtest::OAMDMA::write(uint16_t addr, uint8_t val, gbtest::BusRequestSource 
 {
     // OAM DMA only uses address FF46h
     if (addr == 0xFF46) {
-        // TODO: Start a transfer
+        // Start a transfer with the given value as the source address MSB
+        startTransfer(val);
+
         return true;
     }
 
@@ -29,12 +78,24 @@ bool gbtest::OAMDMA::write(uint16_t addr, uint8_t val, gbtest::BusRequestSource 
 
 bool gbtest::OAMDMA::readOverride(uint16_t addr, uint8_t& val, gbtest::BusRequestSource requestSource) const
 {
-    // TODO: Override when a DMA transfer is in progress
+    // Override when a DMA transfer is in progress and the requested address is not in the HRAM region
+    if (m_transferring && (addr < 0xFF80 || addr > 0xFFFE)) {
+        // "Read" the value as FFh
+        val = 0xFF;
+
+        return true;
+    }
+
     return false;
 }
 
 bool gbtest::OAMDMA::writeOverride(uint16_t addr, uint8_t val, gbtest::BusRequestSource requestSource)
 {
-    // TODO: Override when a DMA transfer is in progress
+    // Override when a DMA transfer is in progress and the requested address is not in the HRAM region
+    if (m_transferring && (addr < 0xFF80 || addr > 0xFFFE)) {
+        // Don't do anything
+        return true;
+    }
+
     return false;
 }
--- src/ppu/oam/OAMDMA.h
+++ src/ppu/oam/OAMDMA.h
@@ -1,6 +1,7 @@
 #ifndef GBTEST_OAMDMA_H
 #define GBTEST_OAMDMA_H
 
+#include "../../platform/bus/Bus.h"
 #include "../../platform/bus/BusProvider.h"
 #include "../../utils/Tickable.h"
 
@@ -10,7 +11,11 @@ class OAMDMA
         : public BusProvider, public Tickable {
 
 public:
+    OAMDMA(Bus& bus);
     ~OAMDMA() override = default;
+
+    void startTransfer(uint8_t sourceAddressHigh);
+    [[nodiscard]] bool isTransferring() const;
 
     void tick() override;
 
@@ -19,6 +24,13 @@ public:
 
     bool readOverride(uint16_t addr, uint8_t& val, BusRequestSource requestSource) const override;
     bool writeOverride(uint16_t addr, uint8_t val, BusRequestSource requestSource) override;
+
+private:
+    bool m_transferring;
+    uint8_t m_currentAddressLow;
+    uint8_t m_sourceAddressHigh;
+
+    Bus& m_bus;
 
 }; // class OAMDMA
 
