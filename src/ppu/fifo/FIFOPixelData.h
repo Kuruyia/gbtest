c86831ff9696d4f427eac402c768ca08fdd14dd1
Kuruyia <8174691+kuruyia@users.noreply.github.com>
2021-09-17 21:04:05 +0200

    FIFOPixelData: Add missing entry

--- src/ppu/fifo/FIFOPixelData.h
+++ src/ppu/fifo/FIFOPixelData.h
@@ -6,7 +6,8 @@ namespace gbtest {
 // Data of a pixel in the FIFO
 struct FIFOPixelData {
     uint8_t colorIndex;         // Color index of this pixel
-    uint8_t palette;            // Palette to use for this pixel
+    uint8_t palette;            // Palette to use for this pixel (DMG: Sprite only; CGB: Palette 0 to 7)
+    uint8_t spriteOamIndex;     // Index of the sprite in the OAM (CGB only)
     bool backgroundPriority;    // For sprites: Priority of the sprite over the background
     // (False: Sprite above BG; True: BG colors 1 to 3 above sprite)
 }; // struct FIFOPixelData
