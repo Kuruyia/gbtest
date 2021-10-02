259eb3964dd9efab1c114b568ffbd33279b45e3a
Kuruyia <8174691+kuruyia@users.noreply.github.com>
2021-10-02 21:38:41 +0200

    main.cpp: Allocate the emulator core on the heap

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <array>
 #include <chrono>
 #include <iostream>
+#include <memory>
 
 #include <raylib.h>
 
@@ -11,14 +12,16 @@ static constexpr float FRAME_TIME = 1.f / TARGET_FPS;
 
 int main()
 {
+    // Init the core emulator
+    std::unique_ptr<gbtest::GameBoy> gameboy = std::make_unique<gbtest::GameBoy>();
+    gameboy->init();
+
+    // Init the window
     InitWindow(680, 616, "gbtest");
     SetTargetFPS(TARGET_FPS);
 
-    gbtest::GameBoy gameboy;
-    gameboy.init();
-
     Image lcdImage = {
-            &(gameboy.getPpu().getFramebuffer().getRawBuffer().front()),
+            &(gameboy->getPpu().getFramebuffer().getRawBuffer().front()),
             160,
             144,
             1,
@@ -26,7 +29,7 @@ int main()
     };
     Texture2D lcdTex = LoadTextureFromImage(lcdImage);
 
-    gameboy.getPpu().getFramebuffer().setFramebufferReadyCallback(
+    gameboy->getPpu().getFramebuffer().setFramebufferReadyCallback(
             [&](const gbtest::Framebuffer::FramebufferContainer& framebuffer) -> void {
                 // Copy the framebuffer for rendering
                 UpdateTexture(lcdTex, &(framebuffer.front()));
@@ -37,28 +40,28 @@ int main()
         uint8_t currByte;
         unsigned offset = 0;
         while (fread(&currByte, sizeof(currByte), 0x1, gbRom) > 0) {
-            gameboy.getBus().write(offset++, currByte, gbtest::BusRequestSource::Privileged);
+            gameboy->getBus().write(offset++, currByte, gbtest::BusRequestSource::Privileged);
         }
 
         fclose(gbRom);
     }
     else {
-        gameboy.getBus().write(0x100, 0x3E, gbtest::BusRequestSource::Privileged); // LD A, 0xFF
-        gameboy.getBus().write(0x101, 0xFF, gbtest::BusRequestSource::Privileged);
+        gameboy->getBus().write(0x100, 0x3E, gbtest::BusRequestSource::Privileged); // LD A, 0xFF
+        gameboy->getBus().write(0x101, 0xFF, gbtest::BusRequestSource::Privileged);
 
-        gameboy.getBus().write(0x102, 0xC6, gbtest::BusRequestSource::Privileged); // ADD A, 0x01
-        gameboy.getBus().write(0x103, 0x01, gbtest::BusRequestSource::Privileged);
+        gameboy->getBus().write(0x102, 0xC6, gbtest::BusRequestSource::Privileged); // ADD A, 0x01
+        gameboy->getBus().write(0x103, 0x01, gbtest::BusRequestSource::Privileged);
 
-        gameboy.getBus().write(0x110, 0x18, gbtest::BusRequestSource::Privileged); // JR -2
-        gameboy.getBus().write(0x111, -2, gbtest::BusRequestSource::Privileged);
+        gameboy->getBus().write(0x110, 0x18, gbtest::BusRequestSource::Privileged); // JR -2
+        gameboy->getBus().write(0x111, -2, gbtest::BusRequestSource::Privileged);
     }
 
     while (!WindowShouldClose()) {
         // Tick the gameboy
-        gameboy.update(FRAME_TIME);
+        gameboy->update(FRAME_TIME);
 
         // Update the joypad
-        gameboy.getJoypad().updateJoypadState(
+        gameboy->getJoypad().updateJoypadState(
                 IsKeyDown(KEY_RIGHT),
                 IsKeyDown(KEY_LEFT),
                 IsKeyDown(KEY_UP),
